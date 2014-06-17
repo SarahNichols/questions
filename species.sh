2a829510533b35bbf7a3793c9357eff898f455d1
SarahNichols <snichols16@gmail.com>
2014-06-17 13:34:00 -0700

    a few more edits. Reminder that more info is needed.

--- species.sh
+++ species.sh
@@ -1,10 +1,12 @@
 # extract the unique names of the animals from the zoo files
 
 #TODO: add more info on the files. 
+# also list kingdom, phylum, class
+
 for filename in $*
 do 
   echo $filename
   cut -d',' -f2 $filename | sort | uniq
 done
 
-print "hello world"
+print "now we're cooking"
